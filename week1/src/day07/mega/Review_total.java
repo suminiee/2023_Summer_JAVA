59a0a08ca4958fe3070cdf3063e2a17a412cfc54
suminiee <90146604+suminiee@users.noreply.github.com>
2023-07-14 14:38:05 +0900

    Add files via upload

--- week1/src/day07/mega/Review_total.java
+++ week1/src/day07/mega/Review_total.java
@@ -2,8 +2,7 @@ package day07.mega;
 
 class Cpu{
 	int core; //개수
-	Cpu(int core){
-		super();
+	public Cpu(int core){
 		this.core = core;
 	}
 	
@@ -14,11 +13,12 @@ abstract class Computer{ //추상클래스는 일반클래스 + 추상메소드
 	int ramSize;
 	int price;
 	int b_point;
+	
 	public Computer(int cpu, int ramSize, int price, int b_point) {
 		this.cpu = new Cpu(cpu);
 		this.ramSize = ramSize;
 		this.price = price;
-		this.b_point = (int)(price * (b_point / 100));
+		this.b_point =price * b_point / 100;
 	}
 	//능력치로 생성자 만들어보기.
 	abstract void powerOn();
@@ -28,8 +28,8 @@ abstract class Computer{ //추상클래스는 일반클래스 + 추상메소드
 class Notebook extends Computer{
 	//추상매서드 구현, 멋지게 켭니다, 업그레이드하면서 끕니다.
 	//상속받은 Cpu, ramSize, price, b_point 10%
-	Notebook(int core, int gb){
-		super(core, gb, 140, 10);
+	Notebook(int core, int gb, int price, int b_point){
+		super(core, gb, price, b_point);
 	}
 	void powerOn() {
 		System.out.println("멋지게 켭니다");
@@ -42,8 +42,8 @@ class Notebook extends Computer{
 class Pad extends Computer{
 	//추상메서드 구현하기 //총알같이 켭니다 //눈깜작할새 끕니다.
 	//상속받은 cpu, ramSize, price, b_point 5%
-	Pad(int core, int gb){
-		super(core, gb, 160, 5);
+	Pad(int core, int gb, int price, int b_point ){
+		super(core, gb, price, b_point);
 	}
 	void powerOn() {
 		System.out.println("멋지게 켭니다");
@@ -92,8 +92,11 @@ public class Review_total {
 		//Computer[] basket -> notebook, pad => notebook과 pad의 조상인 Computer가 될것임.
 		
 		Consumer man1 = new Consumer(0, 10000);
-		Notebook nb = new Notebook(8,16);
+		Notebook nb = new Notebook(8,16,900,10);
+		man1.purchase(nb);
 		System.out.println(man1);
-		
+		Pad pb = new Pad(8, 15, 500, 5);
+		man1.purchase(pb);
+		System.out.println(man1);
 	}
 }
